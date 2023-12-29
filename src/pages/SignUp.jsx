bc5f165ac7d460887970a85c2321a573617fad83
Huy Tran <87997357+hi-huytran@users.noreply.github.com>
2023-12-29 12:05:09 -0600

    add user to firestore database

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -7,7 +7,8 @@ import {
   createUserWithEmailAndPassword,
   updateProfile,
 } from 'firebase/auth'
-// import { db } from '../firebase.config'
+import { db } from '../firebase.config'
+import { setDoc, doc, serverTimestamp } from 'firebase/firestore'
 
 function SignUp() {
   const [showPassword, setShowPassword] = useState(false)
@@ -31,15 +32,25 @@ function SignUp() {
 
     try {
       const auth = getAuth()
+
       const userCredential = await createUserWithEmailAndPassword(
         auth,
         email,
         password
       )
-      // const user = userCredential.user
+
+      const user = userCredential.user
+
       updateProfile(auth.currentUser, {
         displayName: name,
       })
+
+      const formDataCopy = { ...formData }
+      delete formDataCopy.password
+      formDataCopy.timestamp = serverTimestamp()
+
+      await setDoc(doc(db, 'users', user.uid), formDataCopy)
+
       navigate('/')
     } catch (error) {
       console.log(error)
